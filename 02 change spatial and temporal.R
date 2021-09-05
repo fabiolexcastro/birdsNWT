bad5901774c9841ac171e39267add15d479321f7
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2021-09-05 09:56:59 -0500

    solution of the problems with the table

--- 02 change spatial and temporal.R
+++ 02 change spatial and temporal.R
@@ -36,7 +36,7 @@ see_changes <- function(spc){
     tb <- terra::as.points(tr)
     df <- terra::as.data.frame(x = tb)
     gm <- terra::geom(tb)
-    df <- rbind(gm[,3:4], df)
+    df <- cbind(gm[,3:4], df)
     names(df) <- paste0('x', 'y', paste0('y', yrs))
     df <- as_tibble(df)
     df <- mutate(df, gc = gcm[k])
