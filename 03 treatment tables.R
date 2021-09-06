44ba46bf4cfcd2ca541f5fe0dec3e66a2e4b8f6f
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2021-09-06 17:31:00 -0500

    treatment the tables

---
+++ 03 treatment tables.R
@@ -0,0 +1,27 @@
+
+
+# Load libraries ----------------------------------------------------------
+require(pacman)
+pacman::p_load(raster, rgdal, rgeos, reproducible, terra, stringr, glue, sf, tidyverse, RStoolbox, fs, fst, trend)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+
+# Load data ---------------------------------------------------------------
+root <- './outputs'
+dirs <- fs::dir_ls(root, type = 'directory')
+spcs <- basename(dirs)
+
+# Function to use ---------------------------------------------------------
+see_changes <- function(spc){
+  
+  spc <- spcs[1]
+  
+  cat('To start ', spc, '\n')
+  
+  
+}
+
+# Apply the function ------------------------------------------------------
+
+
