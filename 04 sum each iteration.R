40a1bc698538539cd875df1707d7a597831b9200
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2021-09-20 16:38:44 -0500

    sum

--- 04 sum each iteration.R
+++ 04 sum each iteration.R
@@ -13,8 +13,19 @@ root <- './inputs/predictions'
 dirs <- fs::dir_ls(root, type = 'directory')
 spcs <- basename(dirs)
 limt <- sf::st_read('limiteareadeestudio.shp')
+targetCRS <- paste('')
+limt <- st_transform(x = limt, targetCRS)
 
 # Function to use ---------------------------------------------------------
-
+make_sum <- function(spc){
+  
+  spc <- spcs[1] # Run and erase
+  
+  cat('Start ', spc, '\n')
+  dir <- grep(spcs, dirs, value = TRUE)
+  fls <- fs::dir_ls(dir, directory = '.tif$')
+  
+  
+}
 
 
