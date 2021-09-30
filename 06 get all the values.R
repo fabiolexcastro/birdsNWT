74eb9cabcf0f514820c050d8393a7974894c4432
fabiolexcastro <fabioalexandercastro@gmail.com>
2021-09-30 17:05:47 -0500

    values

---
+++ 06 get all the values.R
@@ -0,0 +1,23 @@
+
+# Load libraries ----------------------------------------------------------
+require(pacman)
+pacman::p_load(raster, rgdal, rgeos, future, furrr, reproducible, RColorBrewer, 
+               colorspaces, ggspatial, ggpubr, gridExtra, terra, stringr, glue, 
+               sf, tidyverse, RStoolbox, fs, future.apply, fst, trend)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+
+# Load data ---------------------------------------------------------------
+root <- './inputs/predictions'
+spcs <- dir_ls(root)
+
+# Get values  -------------------------------------------------------------
+get_extreme_values <- function(spc){
+  
+  spc <- spcs[1]
+  
+  f
+  
+}
+
