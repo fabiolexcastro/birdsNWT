587b861613e143e359da5422346e2c2448fc88e7
fabiolexcastro <fabioalexandercastro@gmail.com>
2021-10-03 10:29:56 -0500

    update

--- 04 sum each iteration.R
+++ 04 sum each iteration.R
@@ -16,6 +16,7 @@ limt <- sf::st_read('limiteareadeestudio.shp')
 targetCRS <-  paste("+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95",
                     "+x_0=0 +y_0=0 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
 limt <- st_transform(x = limt, targetCRS)
+qntl <- readRDS(file = './outputs/rds/all_qntl.rds')
 
 # Function to use ---------------------------------------------------------
 make_sum <- function(spc){
