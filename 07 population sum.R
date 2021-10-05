11ae13376d574195ecd339e4cd34e7bed975f5fa
fabiolexcastro <fabioalexandercastro@gmail.com>
2021-10-05 16:41:19 -0500

    update

--- 07 population sum.R
+++ 07 population sum.R
@@ -80,7 +80,7 @@ make_graph <- function(spc){
   tbl <- mutate(tbl, period = factor(period, levels = c('2011', '2031', '2051', '2071', '2091', '2100')))
   tbl <- mutate(tbl, run = factor(run, levels = c('1', '2', '3', '4', '5')))
   
-  ggg <- ggplot(data = tbl, aes(x = period, y = sum_pop, fill = run)) + 
+  ggg <- ggplot(data = tbl, aes(x = period, y = sum_pop / 1000000, fill = run)) + 
     geom_bar(stat = 'identity', position = position_dodge()) + 
     facet_wrap(.~model, nrow = 1, ncol = 3) +
     scale_fill_manual(values = c("#D43F3A", "#EEA236", "#5CB85C", "#46B8DA", "#9632B8")) +
@@ -89,7 +89,7 @@ make_graph <- function(spc){
     theme(legend.position = 'bottom', 
           axis.text.y = element_text(angle = 90, vjust = 0.5, hjust = 0.5), 
           plot.title = element_text(size = 16, hjust = 0.5, face = 'bold')) +
-    labs(x = '', y = 'Total population', fill = 'Run')
+    labs(x = '', y = 'Total population (x 1000000)', fill = 'Run')
   
   
   
