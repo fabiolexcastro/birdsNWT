732617ea96cb2d92b9b9bce63288ea7c2669eb3c
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2021-12-12 11:06:10 -0500

    update

---
+++ 17 make sum raster.R
@@ -0,0 +1,17 @@
+
+
+makeSum <- function(rasterStack){
+  rasList = lapply(X = meanStack, FUN = function(stack){
+    sumRas <- lapply(stack, FUN = function(x){
+      message(crayon::blue('Calculating sum'))
+      ras <- cellStats(x, sum, na.rm = TRUE) 
+    })
+    return(sumRas)
+  })
+  return(rasList)
+} 
+
+
+data <- a
+a
+flatten(a)
