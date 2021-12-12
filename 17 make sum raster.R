bec820a8d7e0a189b1ab899630f8c0f2bbbbbd06
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2021-12-12 11:56:27 -0500

    making the nice graph

--- 17 make sum raster.R
+++ 17 make sum raster.R
@@ -1,4 +1,8 @@
 
+# Load libraries
+require(pacman)
+p_load(raster, rgdal, rgeos, stringr, tidyverse, qs, fs)
+
 
 makeSum <- function(rasterStack){
   rasList = lapply(X = meanStack, FUN = function(stack){
@@ -52,11 +56,50 @@ data <- data |>
   spread(year, value) |> 
   setNames(c('specie', 'gcm', 'y2011', 'y2091'))
 
-gsct <- ggplot(data = data |> filter(specie == 'ALFL'), aes(x = y2011, y = y2091)) 
+data <- qs::qread(file = '../qs/totalAbundance1191.qs')
 
 
+# Functions ---------------------------------------------------------------
+make_graph <- function(gcm1, gcm2){
+  
+  # gcm1 <- 'CanESM2'
+  # gcm2 <- 'CCSM4'
+  
+  cat('Start\n')
+  tble <- data |> filter(gcm %in% c(gcm1, gcm2))
+  corl <- tble |> 
+    group_by(gcm) |> 
+    summarise(corr = cor(y2011, y2091, method = 'pearson')) |> 
+    ungroup() |> 
+    mutate(corr = round(corr, 2))
+  
+  cat('Making the graph\n')
+  gsct <- ggplot(data = tble, 
+                 aes(x = y2011, y = y2091, col = gcm)) + 
+    geom_point() + 
+    scale_color_manual(values = c('#BC679B', '#3E51E3')) + 
+    geom_text(aes(x = 40000000, y = 20000000, label = glue('R = {corl[1,2]}')), col = '#BC679B') +
+    geom_text(aes(x = 40000000, y = 19000000, label = glue('R = {corl[2,2]}')), col = '#3E51E3') +
+    geom_smooth(method = 'lm', se = TRUE) +
+    theme_ipsum_es() + 
+    theme(legend.position = 'bottom', 
+          axis.text.y = element_text(angle = 90, vjust = 0.5,  hjust = 0.5)) + 
+    labs(x = 2011, y = 2091, col = 'GCM')
+  
+  return(gsct)
+  
+  
+}
 
+# Apply the function ------------------------------------------------------
+gcms <- unique(data$gcm)
+g1g2 <- make_graph(gcm1 = gcms[1], gcm2 = gcms[2])
+g1g3 <- make_graph(gcm1 = gcms[1], gcm2 = gcms[3])
+g2g3 <- make_graph(gcm1 = gcms[2], gcm2 = gcms[3])
 
+# Join all into only one
+gall <- ggpubr::ggarrange(g1g2, g2g3, g2g3, ncol = 1, nrow = 3)
+ggsave(plot = gall, filename = '../png/corr_graph.jpg', units = 'in', width = 9, height = 17, dpi = 300)
 
 
 
