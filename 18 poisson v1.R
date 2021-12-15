c43986b998bbb7d9579e55b548fce54334a2d475
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2021-12-15 16:37:49 -0500

    Poisson to each raster

---
+++ 18 poisson v1.R
@@ -0,0 +1,36 @@
+
+
+# Load libraries ----------------------------------------------------------
+require(pacman)
+pacman::p_load(raster, rcartocolor, rgdal, rgeos, future, furrr, reproducible, RColorBrewer, 
+               colorspace, ggspatial, ggpubr, gridExtra, hrbrthemes, terra, stringr, glue, 
+               sf, tidyverse, RStoolbox, fs, future.apply, fst, trend, crayon)
+
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999)
+
+# Load data ---------------------------------------------------------------
+root <- './outputs'
+spcs <- dir_ls(root, type = 'directory')
+
+
+# Function -----------------------------------------------------------------
+get_pssn <- function(spc){
+  
+  spc <- spcs[1] # Run and erase
+  
+  cat('Start ', scp, '\n')
+  fls <- dir_ls(spc)
+  
+  # Testing CanESM2
+  fle <- grep('CanESM2', fls, value = TRUE)
+  fle <- grep('2011', fle, value = TRUE)
+  rst <- raster(fle)
+  
+  calc()
+  
+  
+}
+
