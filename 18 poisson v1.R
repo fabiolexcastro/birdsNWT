3bf13c73d34be38de0c85a57688ade89d30f67e9
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2021-12-15 16:42:44 -0500

    update

--- 18 poisson v1.R
+++ 18 poisson v1.R
@@ -28,6 +28,7 @@ get_pssn <- function(spc){
   fle <- grep('CanESM2', fls, value = TRUE)
   fle <- grep('mean', fle, value = TRUE)
   fle <- grep('2011', fle, value = TRUE)
+  fle <- as.character(fle)
   rst <- raster(fle)
   
   calc()
