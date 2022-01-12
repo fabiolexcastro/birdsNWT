8bf631445d4830b86c79924e79dfa67f973e0c8e
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-01-12 13:32:54 -0500

    analaisis muestreo v1

--- 19 muestreo v1.r
+++ 19 muestreo v1.r
@@ -13,13 +13,19 @@ nmrs <- read_csv('')[,1:4] # Leer la tabla de las proporciones de area por pixel
 mask <- raster('') # Leer el raster de los poligonos 
 shpf <- st_read('') # Leer el shape de las ecoregiones
 shpf$gid <- 1:nrow(shpf)
-
 crs(mask) <- targetCRS
+znes <- unique(shpf$gid)
 
 # Function to use ----------------------------
-make_sample <- function(){
+make_sample <- function(zne){
+  
+  zne <- 1
+  cat('Start ', zne, '\n')
+  nmr <- filter(nmrs, PolyID == zne)
+  head(nmr)
   
   
+  cat('Done!\n')
   
   
 }
