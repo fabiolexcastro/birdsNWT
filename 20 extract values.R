87319075e1028b1b209f1ff4449feda6920dfc5c
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-01-13 11:49:40 -0500

    Update

---
+++ 20 extract values.R
@@ -0,0 +1,16 @@
+
+
+# Load libraries ----------------------------
+require(pacman)
+pacman::p_load(raster, rgdal, rgeos, stringr, sf, fasterize, tidyverse, fs, gtools, glue)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999)
+
+# Load data ---------------------------------------------------------------
+fles <- dir_ls('./qs/sample')
+tbls <- map(fles, qread)
+
+
+
