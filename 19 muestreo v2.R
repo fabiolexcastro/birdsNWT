4b851f9e7b8a37b01701257c62f03a58bbf8cdaa
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-01-13 16:16:46 -0500

    cell numbers

---
+++ 19 muestreo v2.R
@@ -0,0 +1,33 @@
+
+
+# Load libraries ----------------------------
+require(pacman)
+pacman::p_load(raster, rgdal, rgeos, stringr, sf, fasterize, tidyverse, fs, gtools, glue)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999)
+
+# Load data ---------------------------------
+mask <- raster('')
+shpf <- st_read('') 
+shpf$gid <- 1:nrow(shpf)
+crs(mask) <- targetCRS
+
+# Fasterize 
+mask <- mask * 0 
+fstr <- fasterize::fasterize(shpf, mask, field = 'gid')
+znes <- sort(as.numeric(na.omit(unique(fstr[]))))
+
+# Getting the sample n
+cntr <- fstr %>% 
+  rasterToPoints() %>% 
+  as_tibble() %>% 
+  setNames(c('x', 'y', 'value')) %>% 
+  group_by(value) %>% 
+  summarise(count = n()) %>% 
+  ungroup() %>%
+  mutate(porc = count / sum(count) * 100, 
+         n = (count * porc) / 100)
+nmrs <- cntr
+head(cntr)
