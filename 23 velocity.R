f670b2949bee0197516340fe09ee8aec70423ea1
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-02-15 14:38:53 -0500

    a

--- 23 velocity.R
+++ 23 velocity.R
@@ -26,6 +26,21 @@ get_velocity <- function(sp){
   flsCur <- dir_ls(flsCur)
   flsCur <- grep('NA_range.tif', flsCur, value = TRUE)
   
+  map(.x = 1:length(flsPrd), .f = function(i){
+    
+    i <- 1 # Run and erase
+    flePrd <- flsPrd[i]
+    rstCur <- raster::raster(fleCur)
+    rstPrd <- raster::raster(flePrd)
+    
+    par(mfrow = c(1, 2))
+    plot(rstCur)
+    plot(rstPrd)
+    par(mfrow = c(1, 1))
+    
+    
+  })
+  
   fls <- fs::dir_ls(sp)
   yrs <- parse_number(basename(fls))
   yrs <- unique(yrs)
