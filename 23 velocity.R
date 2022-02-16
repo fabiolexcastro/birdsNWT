9df63ff1c53cc509387fb64f31f5704944d27ae5
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-02-16 14:17:34 -0500

    update

--- 23 velocity.R
+++ 23 velocity.R
@@ -45,8 +45,8 @@ get_velocity <- function(sp){
     head(p.xy)
     head(f.xy)
     
-    p.xy2 <- filter(p.xy, prev > 0.1) %>% dplyr::select(1:3)
-    f.xy2 <- filter(f.xy, prev > 0.1) %>% dplyr::select(1:3)
+    p.xy2 <- as.data.frame(p.xy) %>% filter(p.xy, prev > 0.1) %>% dplyr::select(1:3) %>% as.matrix()
+    f.xy2 <- as.data.frame(f.xy) %>% filter(f.xy, prev > 0.1) %>% dplyr::select(1:3) %>% as.matrix()
   
     
     
